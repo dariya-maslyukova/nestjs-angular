3c718b869355dffabadb3250902c5c5a9565c9cc
Dariya Maslyukova <darija0207@ukr.net>
2019-04-10 14:39:19 +0300

    Footer Component

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,34 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { NavItem } from '../../interfaces/nav/nav-item.interface';
+import { emailValidator } from '../../validators/email.validator';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
 
   navItems: NavItem[] = [
     {
-      label: 'Collection',
-      route: '/collection/look',
+      label: 'Deliveries',
+      route: '/deliveries',
     },
     {
-      label: 'Women',
-      route: '/collection/women',
+      label: 'Returns',
+      route: '/returns',
     },
     {
-      label: 'Men',
-      route: '/collection/men',
+      label: 'Terms and conditions',
+      route: '/terms-conditions',
     },
     {
-      label: 'Sale',
-      route: '/collection/sale',
+      label: 'Affiliate Program',
+      route: '/affiliate-program',
+    },
+    {
+      label: 'Contact Us',
+      route: '/contact',
     },
   ];
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private us: UtilsService
+  ) {
+
+    this.form = this.fb.group({
+      Email: [
+        null,
+        [
+          Validators.required,
+          emailValidator,
+        ],
+      ],
+    });
+  }
 
   ngOnInit() {
   }
+
+  onSubmit(event?: Event): void {
+    if (event) {
+      event.preventDefault();
+    }
+
+    if (!this.form.valid) {
+      return this.us.validateAllFormFields(this.form);
+    }
+  }
+
 
 }
